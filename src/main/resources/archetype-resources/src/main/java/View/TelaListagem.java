2836ca23691144d5fa7a3df6db080e3c7cf65b90
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2022-09-07 16:29:13 -0300

    Update TelaListagem.java

--- src/main/resources/archetype-resources/src/main/java/View/TelaListagem.java
+++ src/main/resources/archetype-resources/src/main/java/View/TelaListagem.java
@@ -32,6 +32,9 @@ public class TelaListagem extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextAreaDadosEventos1 = new javax.swing.JTextArea();
         jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jTextFieldBuscar = new javax.swing.JTextField();
+        jButtonBuscar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(1000, 900));
@@ -80,12 +83,30 @@ public class TelaListagem extends javax.swing.JFrame {
         jScrollPane2.setViewportView(jTextAreaDadosEventos1);
 
         getContentPane().add(jScrollPane2);
-        jScrollPane2.setBounds(80, 160, 740, 570);
+        jScrollPane2.setBounds(80, 270, 740, 460);
 
         jLabel8.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel8.setText("ATIVIDADES CADASTRADAS:");
+        jLabel8.setText("BUSCAR:");
         getContentPane().add(jLabel8);
-        jLabel8.setBounds(50, 120, 250, 25);
+        jLabel8.setBounds(30, 100, 90, 25);
+
+        jLabel9.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel9.setText("ATIVIDADES CADASTRADAS:");
+        getContentPane().add(jLabel9);
+        jLabel9.setBounds(30, 230, 250, 25);
+        getContentPane().add(jTextFieldBuscar);
+        jTextFieldBuscar.setBounds(80, 140, 740, 30);
+
+        jButtonBuscar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButtonBuscar.setText("BUSCAR");
+        jButtonBuscar.setToolTipText("");
+        jButtonBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonBuscarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButtonBuscar);
+        jButtonBuscar.setBounds(720, 190, 100, 40);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -101,6 +122,10 @@ public class TelaListagem extends javax.swing.JFrame {
     private void jButtonVoltarNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonVoltarNomeActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButtonVoltarNomeActionPerformed
+
+    private void jButtonBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonBuscarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonBuscarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -139,12 +164,15 @@ public class TelaListagem extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonBuscar;
     private javax.swing.JButton jButtonDeletarNome;
     private javax.swing.JButton jButtonEditarNome;
     private javax.swing.JButton jButtonVoltarNome;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea jTextAreaDadosEventos1;
+    private javax.swing.JTextField jTextFieldBuscar;
     // End of variables declaration//GEN-END:variables
 }
