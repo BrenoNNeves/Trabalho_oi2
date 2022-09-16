9d5c56656b3c3cffd3047b8c87d1b759093fba12
BrenoNNeves <brebre.iasd2@hotmail.com>
2022-09-16 19:58:58 -0300

    pequenas altera

---
+++ src/main/resources/archetype-resources/src/main/java/DAO/atividadeDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package archetype
+
+-resources.src.main.java.DAO;
+
+/**
+ *
+ * @author brebr
+ */
+public class atividadeDAO {
+    
+}
