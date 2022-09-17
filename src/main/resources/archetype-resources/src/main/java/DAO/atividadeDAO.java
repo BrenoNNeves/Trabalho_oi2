866121cdaa7d05b563ff79b86615523b936f350e
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2022-09-17 09:39:59 -0300

    DAO Concluida
    DAO Concluida

--- src/main/resources/archetype-resources/src/main/java/DAO/atividadeDAO.java
+++ src/main/resources/archetype-resources/src/main/java/DAO/atividadeDAO.java
@@ -1,8 +1,244 @@
 package DAO;
-/**
- *
- * @author brebr
- */
-public class AtividadeDAO {
+
+
+import CadastroLista;
+import archetype-resources.src.main.java.Model;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class atividadeDAO{
+    
+     public static void salvar(CadastroLista cadastro) 
+        throws SQLException, Exception {
+        
+        String sql = "INSERT INTO atividade.cad(nome, dat, "
+                + "materia, concluido) "
+                + "VALUES(?, ?, ?, ?);";
+            
+            Connection connection = null;
+            PreparedStatement preparedStatement = null;
+        
+            try {
+            
+            connection = ConnectionUtils.obterConexao();
+           
+            preparedStatement = connection.prepareStatement(sql);
+            
+            preparedStatement.setString(1, cadastro.getNomeAtv());
+            preparedStatement.setString(2, cadastro.getDataAtv());
+            preparedStatement.setString(3, cadastro.getMateriaAtv());
+            preparedStatement.setBoolean(4, cadastro.isStatus());
+            
+            preparedStatement.execute();
+        } finally {
+            
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+
+    public static List<CadastroLista> listar()
+        throws SQLException, Exception {
+        
+        String sql = "SELECT  id, nome, dat, materia, concluido "
+                + "FROM atividade.cad";
+        List<Cadastro> listaClientes = null;
+        
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet result = null;
+        
+        try{
+            connection = ConnectionUtils.obterConexao();
+            preparedStatement = connection.prepareStatement(sql);
+            result = preparedStatement.executeQuery();
+            
+            while (result.next()) {
+                if (listaClientes == null) {
+                    listaClientes = new ArrayList<Cadastro>();
+                }
+                CadastroLista cadastro = new CadastroLista();
+                cadastro.IdAtv(result.getInt("id"));
+                cadastro.setNomeAtv(result.getString("nome"));
+                cadastro.setMateriaAtv(result.getString("materia"));
+                cadastro.setDataAtv(result.getString("dat"));
+                cadastro.setStatus(result.getString("concluido"));
+             
+                listaClientes.add(cadastro);
+            }
+        }finally{
+            if(result != null && !result.isClosed()){
+                result.close();
+            }
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+        return listaClientes;
+    }
+
+    public static List<CadastroLista> procurar(String nome)
+        throws SQLException, Exception {
+        
+        String sql = "SELECT  id, nome, dat, materia, concluido"
+                + "FROM atividade.cad WHERE nome LIKE (?)";
+        List<CadastroLista> listaClientes = null;
+        
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet result = null;        
+        
+        try{
+            connection = ConnectionUtils.obterConexao();
+            preparedStatement = connection.prepareStatement(sql);
+            
+            preparedStatement.setString(1, "%" + nome + "%");
+            
+            result = preparedStatement.executeQuery();
+            
+            while (result.next()) {
+                if (listaClientes == null) {
+                    listaClientes = new ArrayList<CadastroLista>();
+                }
+                CadastroLista cadastro = new CadastroLista();
+                
+                cadastro.setIdAtv(result.getInt("id"));
+                cadastro.setNomeAtv(result.getString("nome"));
+                cadastro.setDataAtv(result.getString("dat"));
+                cadastro.setMateriaAtv(result.getString("materia"));
+                cadastro.setStatus(result.getString("concluido"));
+                
+                listaClientes.add(cadastro);
+            }
+        }finally{
+            if(result != null && !result.isClosed()){
+                result.close();
+            }
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+        return listaClientes;
+    }
+
+    public static CadastroLista obter(Integer id) 
+        throws SQLException, Exception {
+        
+        String sql = "SELECT * FROM atividade.cad WHERE id =?";
+        
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet result = null;
+        
+        try {
+            connection = ConnectionUtils.obterConexao();
+            preparedStatement = connection.prepareStatement(sql);
+            
+            preparedStatement.setInt(1, id);
+            result = preparedStatement.executeQuery();
+            
+            if(result.next()){
+                CadastroLista cadastro = new CadastroLista();
+                
+                cadastro.setIdAtv(result.getInt("id"));
+                cadastro.setNomeAtv(result.getString("nome"));
+                cadastro.setDataAtv(result.getString("dat"));
+                cadastro.setMateriaAtv(result.getString("materia"));
+                cadastro.setStatus(result.getString("concluido"));       
+                
+                return cadastro;
+            }
+        }finally{
+            if(result != null && !result.isClosed()){
+                result.close();
+            }
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+        return null;
+    } 
+    
+    public static void atualizar(CadastroLista cadastroAtualizado) 
+        throws SQLException, Exception {
+        
+        String sql = "UPDATE atividade.cad SET nome = ?, dat = ?, "
+                + "materia = ?, concluido = ?"
+                + "WHERE id = ?;";
+        
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        
+        try {
+            
+            connection = ConnectionUtils.obterConexao();
+            preparedStatement = connection.prepareStatement(sql);
+            
+            preparedStatement.setInt(1, cadastroAtualizado.getId());
+            preparedStatement.setString(1, cadastroAtualizado.getNomeAtv());
+            preparedStatement.setString(2, cadastroAtualizado.getDataAtv());
+            preparedStatement.setString(3, cadastroAtualizado.getMateriaAtv());
+            preparedStatement.setString(4, cadastroAtualizado.getStatusAtv());            
+
+            //Executa o comando no banco de dados
+            preparedStatement.executeUpdate();
+        } finally {
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+    
+    public static void excluir(Integer id)
+        throws SQLException, Exception {
+        String sql = "DELETE FROM atividade.cad WHERE id = ?";        
+             
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        
+        try {
+            connection = ConnectionUtils.obterConexao();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+
+            //Executa o comando no banco de dados
+            preparedStatement.executeUpdate();
+        } finally {
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+
     
 }
+
