af76edb80b6fdb5e8f245f6f21b02fbec59776fc
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2022-09-17 08:53:09 -0300

    Update atividadeDAO.java

--- src/main/resources/archetype-resources/src/main/java/DAO/atividadeDAO.java
+++ src/main/resources/archetype-resources/src/main/java/DAO/atividadeDAO.java
@@ -2,14 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package archetype
-
--resources.src.main.java.DAO;
-
+package DAO;
 /**
  *
  * @author brebr
  */
-public class atividadeDAO {
+public class AtividadeDAO {
     
 }
